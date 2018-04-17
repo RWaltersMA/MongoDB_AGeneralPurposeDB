d24a446a28069b741a4f685efa936edc3d344d38
fullaware <>
2018-04-17 15:23:02 -0400

    copies the latitude/longitude into `location` object for 2dsphere indexing

--- config/yelp_prep.js
+++ config/yelp_prep.js
@@ -18,29 +18,18 @@ var connectionString = "mongodb://localhost:27017";
 MongoClient.connect(connectionString, function (err, db) {
     if (err) throw err;
     var dbo = db.db("yelp");
-    var ResultSet = [];
+
     dbo.collection("business").find({ "location": { $exists: false } })
     .project({"_id":1, longitude:1, latitude:1})
-    .limit(10)
-    .toArray()
-    .then(function (items) {
-        items.forEach(function (item, idx, array) 
+    .forEach(function (document) 
             {       
-                ResultSet.push({
-                        _id : item["_id"],
-                        longitude: item["longitude"],
-                        latitude: item["latitude"]});
-                dbo.collection("business").update({"_id": ObjectId(item["_id"])}, 
+                console.log(document);
+                dbo.collection("business").update({"_id": ObjectId(document._id)}, 
                     { $set: 
                         {
                             "location.type": "Point",
-                            "location.coordinates":[item["latitude"],item["longitude"]]
+                            "location.coordinates":[document.latitude,document.longitude]
                         }
                     });
-            });
-        
-        
-        console.log(ResultSet);
-        db.close();
-      });
+                });
     });
