aa94a9f6f9d377102f5f0e6ba59d4baa871ac776
fullaware <>
2018-05-02 13:00:13 -0400

    init

--- routes/GraphSearch.js
+++ routes/GraphSearch.js
@@ -15,7 +15,7 @@ router.get('/', function (req,res,next) {
     //Enumerate db.users.find( { $and: [{ $where: "this.friends.length < 4"}, { $where: "this.friends.length > 1"} ]} ).limit(10);
     //Fill an object to pass to UI
     
-     //If they jumped directly to a route and don't have a sessionID redirect them
+    //If they jumped directly to a route and don't have a sessionID redirect them
     if (!req.session.sessionID) 
     {
         res.redirect('/');
@@ -23,83 +23,83 @@ router.get('/', function (req,res,next) {
         return;
     }
 
-MongoClient.connect(settings.connectionString, function(err, client) {
+    MongoClient.connect(settings.connectionString, function(err, client) {
 
-    assert.equal(null, err);
-    if(err) throw err;
+        assert.equal(null, err);
+        if(err) throw err;
 
-    db = client.db(settings.database);
-    var ResultSet=[];
+        db = client.db(settings.database);
+        var ResultSet=[];
 
-    var TextSearchResults = db.collection("users").find({
-        "$and": [
-    {"$where": "this.friends.length < 5"}, { $where: "this.friends.length > 1"}]}).limit(10).toArray().then(function (items) { 
+        var TextSearchResults = db.collection('users').find({
+            '$and': [
+                {'$where': 'this.friends.length < 5'}, { $where: 'this.friends.length > 2'}]}).limit(10).toArray().then(function (items) { 
  
             items.forEach((item, idx, array) => 
             {       
                 ResultSet.push({
-                        name: item['name'],
-                        number_of_reviews: item['review_count'],
-                        user_id: item['user_id'],
-                        friends: item['friends']});
+                    name: item['name'],
+                    number_of_reviews: item['review_count'],
+                    user_id: item['user_id'],
+                    friends: item['friends']});
 
             });
 
             //res.send(ResultSet); // sendStatus(201);
-           // res.end();
+            // res.end();
             client.close();
             res.render('graph', { ResultSet });
         })
-    .catch(function(e) {
-            console.log(e);
-             res.status(500).send(e);
-});
-}); //MongoClient
+            .catch(function(e) {
+                console.log(e);
+                res.status(500).send(e);
+            });
+    }); //MongoClient
 });
 
 router.post('/', function (req,res, next){
 
-var ResultSet=[];
+    var ResultSet=[];
 
-var UserIDToQuery=req.body.UserID;
-var LevelsToQuery=req.body.Levels;
+    var UserIDToQuery=req.body.UserID;
+    var LevelsToQuery=req.body.Levels;
 
 
-MongoClient.connect(settings.connectionString, function(err, client) {
+    MongoClient.connect(settings.connectionString, function(err, client) {
 
-    assert.equal(null, err);
-    if(err) throw err;
+        assert.equal(null, err);
+        if(err) throw err;
 
-    db = client.db(settings.database);
+        db = client.db(settings.database);
 
-    var TextSearchResults = db.collection("users").aggregate([
-        { '$match': {
+        var TextSearchResults = db.collection('users').aggregate([
+            { '$match': {
                 'user_id' : UserIDToQuery }
-        },
-        {'$graphLookup':
+            },
+            {'$graphLookup':
          { 'from': 'users',
-                'startWith': [ UserIDToQuery] ,
-                'connectFromField': 'friends',
-                'connectToField': 'user_id',
-                'as': 'socialNetwork',
-                'maxDepth': Number(LevelsToQuery),
-                'depthField':'depth'
-        }
-        }
-        ,
-      { '$project':
+             'startWith': [ UserIDToQuery] ,
+             'connectFromField': 'friends',
+             'connectToField': 'user_id',
+             'as': 'socialNetwork',
+             'maxDepth': Number(LevelsToQuery),
+             'depthField':'depth'
+         }
+            }
+            ,
+            { '$project':
           {     '_id':0,
-                'name':1,
-                'Network':'$socialNetwork.name',
-                'Depth':'$socialNetwork.depth'
-    }}]).toArray().then(function (items) { 
+              'name':1,
+              'Network':'$socialNetwork.name',
+              'Depth':'$socialNetwork.depth'
+          }}]).toArray().then(function (items) { 
  
             items.forEach((item, idx, array) => 
             {       
                 ResultSet.push({
-                       // name: item['name'],
-                        depth: item['Depth'],
-                        network: item['Network']});
+                    // name: item['name'],
+                    depth: item['Depth'],
+                    network: item['Network']});
 
             });
 
@@ -107,12 +107,12 @@ MongoClient.connect(settings.connectionString, function(err, client) {
             res.end();
             client.close();
         })
-    .catch(function(e) {
-         res.status(500).send(e);
-            console.log(e);
-});
-}); //MongoClient
+            .catch(function(e) {
+                res.status(500).send(e);
+                console.log(e);
+            });
+    }); //MongoClient
 
-})
+});
 
 module.exports = router;
